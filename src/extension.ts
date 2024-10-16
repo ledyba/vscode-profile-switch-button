d79e6d589876d30ddb4d419998d0458456ca2c35
Kaede Fujisaki <kaede@hexe.net>
2024-10-16 23:07:01 +0900

    Switch profile

--- src/extension.ts
+++ src/extension.ts
@@ -2,24 +2,17 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+let statusBar: vscode.StatusBarItem;
+const COMMAND_NAME = 'workbench.profiles.actions.switchProfile';
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "vscode-profile-switcher" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('vscode-profile-switcher.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-profile-switcher!');
-	});
-
-	context.subscriptions.push(disposable);
+  statusBar = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+  statusBar.command = COMMAND_NAME;
+	statusBar.text = 'Switch Profile';
+  statusBar.show();
+  context.subscriptions.push(statusBar);
 }
 
 // This method is called when your extension is deactivated
